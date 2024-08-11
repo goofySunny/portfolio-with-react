b0e58b404275334324ec671d8e2a277c1b16cd35
Ali <alinajafian580@Gmail.com>
2024-08-11 21:56:44 +0330

    Custom color pallete configured and used (so I can make light mode later)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import Logo from '../../assets/images/logo.svg';
 import DownArrow from '../../assets/images/down-arrow.svg';
+import Github from '../../assets/images/Github.svg';
 import { useState } from 'react';
 
 
@@ -11,23 +12,31 @@ function Header() {
         setlanguage(e.target.innerHTML)
     }
     return (
-        <div className='flex justify-between bg-[#282C33] p-4 items-center container mx-auto'>
+        <div className='flex justify-between bg-background p-4 items-center container mx-auto'>
             <div className='flex items-center'>
                 <img src={Logo} alt="najaf" />
-                <span className='text-[#fff] font-bold mx-3'>Ali</span>
+                <span className='text-white font-bold mx-3'>Ali</span>
             </div>
             <div className='flex flex-row justify-between'>
-                <a href="" className='text-[#ABB2BF] mx-3 hover:bg-[#16181b]'><span className='text-[#C778DD]'>#</span>works</a>
-                <a href="" className='text-[#ABB2BF] mx-3 hover:bg-[#16181b]'><span className='text-[#C778DD]'>#</span>about-me</a>
-                <a href="" className='text-[#ABB2BF] mx-3 hover:bg-[#16181b]'><span className='text-[#C778DD]'>#</span>contacts</a>
-                <a href="" className='text-[#ABB2BF] mx-3 hover:bg-[#16181b]'><span className='text-[#C778DD]'>#</span>home</a>
-                <div className='relative text-[#ABB2BF] ms-2 flex hover:bg-[#16181b] select-none group'>
+                <a href="" className='text-secondary mx-3 hover:bg-activatedBackground'><span className='text-primary'>#</span>works</a>
+                <a href="" className='text-secondary mx-3 hover:bg-activatedBackground'><span className='text-primary'>#</span>about-me</a>
+                <a href="" className='text-secondary mx-3 hover:bg-activatedBackground'><span className='text-primary'>#</span>contacts</a>
+                <a href="" className='text-secondary mx-3 hover:bg-activatedBackground'><span className='text-primary'>#</span>home</a>
+                <div className='relative text-secondary ms-2 flex hover:bg-activatedBackground select-none group'>
                     <span>{language}</span>
                     <img src={DownArrow} alt="" width={15} />
-                    <div className='absolute hidden group-hover:block top-5 bg-[#282C33] p-2'>
+                    <div className='absolute hidden group-hover:block top-5 bg-background p-2'>
                         <button onClick={changeLanguage} className='hover:bg-[#16181b]'>EN</button>
                         <button onClick={changeLanguage} className='hover:bg-[#16181b]'>FA</button>
                     </div>
+                </div>
+            </div>
+            <div className='absolute left-10 top-0'>
+                <div className='absolute left-1/2 border-l border-secondary h-full'></div>
+                <div className='relative flex flex-col mt-20 pt-2 z-10 bg-[#282C33]'>
+                    <img src={Github} alt="" />
+                    <img src={Github} alt="" />
+                    <img src={Github} alt="" />
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'primary': '#C778DD',
+      'secondary': '#ABB2BF',
+      'background': '#282C33',
+      'activatedBackground': '#16181b',
+      'white': '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
