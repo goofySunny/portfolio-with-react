0ca5a8852d978305e11b83304d14b72ff59c24e9
Ali <alinajafian580@Gmail.com>
2024-08-18 14:31:31 +0330

    Divider component added and reusable for later (except for the see more button)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Divider from './components/Divider/Divider';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import Quote from './components/quote/Quote';
@@ -9,6 +10,7 @@ function App() {
     <Header/>
     <Hero/>
     <Quote thingToSay='Turning coffee into code one sip at a time'/>
+    <Divider title='Projects' />
     </>
   )
 }
